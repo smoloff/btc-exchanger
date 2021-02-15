a731703e3148d0394645db2eafddc07b82429391
smoloff <smoloff@gmail.com>
2021-02-15 11:56:46 +0200

    add data to exchangeData.js

---
+++ exhangeData.js
@@ -0,0 +1,27 @@
+let exchangeData [
+    let BNB {
+        name:"BNB",
+        fullname:"BNB",
+        lastPrice: 130.1,
+        Change24h: -2.39
+    }
+    let BTC {
+        name:"BTC",
+        fullname:"Bitcoin",
+        lastPrice: 42921.60,
+        Change24h:-2.39        
+    }
+    let ETH {
+        name: "ETH",
+        fullname: "Etherium",
+        lastPrice: 1800.80,
+        Change24h:0.61
+    }
+
+    let DOGE {
+        name: "DOGE",
+        fullname:"Dogecoin",
+        lastPrice:0.067940,
+        Change24h: -0.99
+    }
+]
